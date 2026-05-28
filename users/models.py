3f6bf63b13b54db0439adae4a94c332b7600b096
safin <safin@example.com>
2026-05-28 23:33:10 +0300

    D.Z.30.1 done

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    city = models.CharField(max_length=50)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
