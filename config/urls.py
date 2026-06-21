8d0adc2ca6c278b82fd3bceaecfc74f70042aa04
safin <safin@example.com>
2026-06-22 00:37:50 +0300

    D.Z.32.2 done

--- config/urls.py
+++ config/urls.py
@@ -46,6 +46,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('materials.urls')),  # Здесь мы подключаем маршруты из приложения materials
+    path('materials/', include('materials.urls')),
     path('', include('users.urls')), # Подключаем маршруты из приложения users
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
